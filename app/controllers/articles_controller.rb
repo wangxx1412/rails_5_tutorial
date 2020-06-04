0e239f4674cb3bdd6a33c231a48acc3f0f93b1a7
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-06-03 20:22:20 -0700

    Finish article resource

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,54 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   # Get Form Route Handler
   def new
   end
 
+  # Get to edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    # use @article to make it available for all actions in this controller
+    @article = Article.find(params[:id])
+  end
+
   # Post Form Route Handler
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to articles_path
+  end
+
+  private
+  # Reuse for multiple actions
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
